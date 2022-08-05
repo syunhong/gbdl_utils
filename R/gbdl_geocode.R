0e771815c05276a6442ad78c03b51849f6e509d5
Seong-Yun Hong <syhong@khu.ac.kr>
2022-08-05 21:05:46 +1200

    Error fix

--- R/gbdl_geocode.R
+++ R/gbdl_geocode.R
@@ -34,8 +34,11 @@ gbdl_geocode <- function(addr, method = "naver", client_id, client_secret,
                            "X-NCP-APIGW-API-KEY" = client_secret))
 
     tmp <- content(tmp, as = "parse", encoding = "UTF-8")
-    if (save_xml)
-      write_xml(tmp, paste0(xml_dir, "/id-", i, ".xml"))
+    if (save_xml) {
+      tryCatch({ 
+        write_xml(tmp, paste0(xml_dir, "/id-", i, ".xml"))
+      }, error = function(e) cat("\nUnable to create a XML file at", i, "\n"))
+    }
     
     tmp_processed <- as_list(tmp)$addressResponse$addresses
     
@@ -49,7 +52,7 @@ gbdl_geocode <- function(addr, method = "naver", client_id, client_secret,
     # If an error occurs:
     tryCatch({ 
       res[i,3:8] <- c(road_addr, jibun_addr, en_addr, x, y, dist)
-    }, error = function(e) cat("\nUnable to geocode at", i, "\n"))
+    }, error = function(e) cat("\nError occurred at", i, "\n"))
     
     if ((i %% autosave_freq) == 0)
       save(res, file = paste0(out_dir, "/res-", i, ".RData"))

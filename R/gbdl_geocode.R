5eb3eeb69b38fdecee19a131d275f98ad6d5bc5f
Seong-Yun Hong <syhong@khu.ac.kr>
2022-08-05 20:41:35 +1200

    Fix error

--- R/gbdl_geocode.R
+++ R/gbdl_geocode.R
@@ -29,20 +29,15 @@ gbdl_geocode <- function(addr, method = "naver", client_id, client_secret,
     if (showProgress)
       setTxtProgressBar(pb, i)
     
-    tmp <- GET(url = url, query = list(query = addr[i]),
+    tmp <- GET(url = url, query = list(query = addr[267]),
                add_headers("X-NCP-APIGW-API-KEY-ID" = client_id,
                            "X-NCP-APIGW-API-KEY" = client_secret))
-    
-    # If an error occurs:
-    tryCatch({ 
-      Sys.sleep(time = 1) 
-    }, error = function(e) cat("\nUnable to geocode at", i, "\n"))
-    
+
     tmp <- content(tmp, as = "parse", encoding = "UTF-8")
     if (save_xml)
       write_xml(tmp, paste0(xml_dir, "/id-", i, ".xml"))
     
-    tmp_processed <- as_list(tmp)$addressResponse$address
+    tmp_processed <- as_list(tmp)$addressResponse$addresses
     
     x <- as.numeric(unlist(tmp_processed$x))
     y <- as.numeric(unlist(tmp_processed$y))
@@ -51,7 +46,10 @@ gbdl_geocode <- function(addr, method = "naver", client_id, client_secret,
     en_addr <- unlist(tmp_processed$englishAddress)
     dist <- as.numeric(unlist(tmp_processed$distance))
     
-    res[i,3:8] <- c(road_addr, jibun_addr, en_addr, x, y, dist)
+    # If an error occurs:
+    tryCatch({ 
+      res[i,3:8] <- c(road_addr, jibun_addr, en_addr, x, y, dist)
+    }, error = function(e) cat("\nUnable to geocode at", i, "\n"))
     
     if ((i %% autosave_freq) == 0)
       save(res, file = paste0(out_dir, "/res-", i, ".RData"))
